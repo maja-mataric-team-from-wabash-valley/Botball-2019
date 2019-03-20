ab39bff4b5c67a5467feedd72d11a19552c046b8
DavidMutchler <mutchler@rose-hulman.edu>
2019-03-20 17:32:40 -0400

    Adds a comment with plan for movement

--- src/lego_main.c
+++ src/lego_main.c
@@ -5,12 +5,39 @@ extern int PRESS_A;
 
 void lego_main() {
     PRESS_A = TRUE;
-
-    forward(12, 100);
-    backward(12, 100);
-    left(90, 50);
+    
+    forward(12, 100);    
+    backward(12, 100);    
+    left(90, 50);    
     right(90, 50);
-
+    
+    /*
+    PLAN FOR MOVEMENT - based on small robot
+    f 26
+    r 90
+    f 13
+    r 90
+    b 2
+    lower basket
+    f 4
+    sweep
+    (f 13
+    sweep)x4
+    basket up
+    f 4
+    l 30
+    
+    l = left
+    r = right
+    f = forward
+    b = backward
+    
+    
+    */
+    
+    
+    //forward(26, 100); right(90, 100)
+    
     /*set_servo_position(0, 1800);
     enable_servo(0);
     press();
